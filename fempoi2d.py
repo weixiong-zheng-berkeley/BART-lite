3fb9c3a5b8d8db5e02496372badcf93a64867da1
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2017-09-19 23:19:00 -0700

    Updated how basis functions are calculated. Still looks wrong - shape is okay, but take a look at amplitude.

--- fempoi2d.py
+++ fempoi2d.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def fempoi2d(h, domain_length):
+def fempoi2d(h, domain_length, t):
     """Solves 2D Poisson's equation for elements of size h 
     and bilinear basis equations"""
     # Define Global Matrices
@@ -16,11 +16,26 @@ def fempoi2d(h, domain_length):
         cell_i = cell//int(np.sqrt(n))
         cell_j = cell%int(np.sqrt(n))
         
-        lower = 1.0/h
-        upper = -1.0/h
         
-        x = np.array([lower, lower, upper, upper]) 
-        y = np.array([lower, upper, lower, upper]) 
+        # Retrieve Values for Nodes of Cell
+        x_lower = t[cell_i, cell_j][1][0]
+        x_upper = t[cell_i, cell_j][1][1]
+        y_lower = t[cell_i, cell_j][1][2]
+        y_upper = t[cell_i, cell_j][1][3]
+        
+        V = np.eye(4)
+        C = np.array([[1, x_lower, y_lower, x_lower*y_lower],
+                      [1, x_lower, y_upper, x_lower*y_upper],
+                      [1, x_upper, y_lower, x_upper*y_lower],
+                      [1, x_upper, y_upper, x_upper*y_upper]])
+        
+        # Solve for Coefficients
+        c1 = np.linalg.inv(C).dot(V[0])
+        c2 = np.linalg.inv(C).dot(V[1])
+        c3 = np.linalg.inv(C).dot(V[2])
+        c4 = np.linalg.inv(C).dot(V[3])
+        c = np.stack((c1, c2, c3, c4), axis=-1)
+        
         
         # Assemble Elementary Matrix
         # A_ab = area(partial_ax*partial_bx + partial_ay*partial_by)
@@ -29,8 +44,12 @@ def fempoi2d(h, domain_length):
         elem_b = np.zeros(4)
         for ii in range(4):
             for jj in range(4):
-                elem_A[ii,jj] = area*(x[ii]*x[jj] + y[ii]*y[jj])
-        elem_b[:]=area/4.0
+                partial_ax = c[1, ii]+c[3, ii]*C[ii, 2]
+                partial_bx = c[1, jj]+c[3, jj]*C[jj, 2]
+                partial_ay = c[2, ii]+c[3, ii]*C[ii, 1]
+                partial_by = c[2, jj]+c[3, jj]*C[jj, 1]
+                elem_A[ii,jj] = area*(partial_ax*partial_bx + partial_ay*partial_by)
+                elem_b[ii]=area/4.0*(4*c[0, ii] + h*(2*c[1, ii]+2*c[2, ii]+h*c[3, ii]))
         
         # Assemble Global Matrix
         # Global to Local node mapping
@@ -58,7 +77,7 @@ def fempoi2d(h, domain_length):
     
     for i in boundary_nodes:
         A[i, i] = 1
-        b[i] = 1   
+        b[i] = 0 #vacuum boundary
         if 0 < i+1 < nodes:
             A[i+1, i] = 0
             A[i-1, i] = 0
@@ -66,7 +85,7 @@ def fempoi2d(h, domain_length):
             A[i+1, i] = 0
         else:
             A[i-1, i] = 0
-            
+
     u = np.linalg.solve(A, b)
     u = u.reshape(nodes_x, nodes_x)
     return u
