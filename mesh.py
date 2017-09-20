4e27cce8ae9954a99e88c522eb88cdb0dd718f49
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2017-09-19 23:17:59 -0700

    Mesh generation method.

---
+++ mesh.py
@@ -0,0 +1,11 @@
+""" Mesh generator for a structured, square, cartesian mesh."""
+import numpy as np
+
+def mesh_gen(cells):
+  xx = np.linspace(0, 10, cells+1)#.reshape(cells, 1)
+  XY = np.zeros((cells+1, cells+1, 2))
+  for i in range(cells+1):
+    for j in range(cells+1):
+        XY[i, j, 0] = xx[i]
+        XY[i, j, 1] = xx[j]
+  return XY
