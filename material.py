9895201fb20cf9efae2b7fe2c4f68d4eca1acee2
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2017-09-24 19:02:57 -0700

    Added estimation of correction coefficients for UA

--- material.py
+++ material.py
@@ -28,7 +28,12 @@ class material(object):
         self.inv_sigt = dict()
 
         # The following is for upscattering acceleration
-        self.ksi = dict()
+        # spectrum
+        self.ksi_ua = dict()
+        # sigt one group
+        self.sigt_ua = dict()
+        # diff_coef one group
+        self.diff_coef_ua = dict()
         # TODO: put whatever else necessary parameters if needed
 
     def read_xsec(self,xsec_filename):
@@ -65,5 +70,37 @@ class material(object):
     def derive_scattering_eigenvalue(self):
         for k, v in self.sigs.items():
             # slicing to get thermal group transfer matrices
-            thermal_scat = v[self.g_thermal::1, self.g_thermal::1]
-            self.ksi[k] = np.linalg.eigvals(thermal_scat)
+            thermal_scat = v[self.g_thermal:, self.g_thermal:]
+            self.ksi_ua[k] = np.linalg.eigvals(thermal_scat)
+
+    def produce_acceleration_properties(self):
+        for k in xrange(n_materials):
+            self.sigt_ua[k] = 0.0
+            self.diff_coef_ua[k] = 0.0
+            self.sigt_ua[k] = np.dot(self.ksi_ua[k], self.sigt[k][self.g_thermal:])
+            self.diff_coef_ua[k] = np.dot(self.ksi_ua[k], self.diff_coef[k][self.g_thermal:])
+
+    def estimate_cell_correction_ua(self, cell_corrections_at_qp, material_id):
+        '''@brief function used to estimate one-group vector_D at quadrature points
+
+        @param cell_corrections_at_qp vector_D for all groups at the specific quadrature
+        point. Use numpy.array
+        @param material_id Materail ID for current cell
+        @return a one-group vector_D in forms of numpy array
+        '''
+        vector_D = np.array([0.0, 0.0])
+        # x component of vector_D
+        vector_D[0] = np.dot(cell_corrections_at_qp[0][self.g_thermal:], self.ksi_ua[material_id])
+        # y component of vector_D
+        vector_D[1] = np.dot(cell_corrections_at_qp[1][self.g_thermal:], self.ksi_ua[material_id])
+        return vector_D
+
+    def estimate_bd_correction_ua(self, bd_corrections_at_qp, material_id):
+        '''@brief function used to estimate one-group kappa at quadrature points
+
+        @param bd_corrections_at_qp kappa for all groups at the specific quadrature
+        point.
+        @param material_id Materail ID for current cell
+        @return a one-group kappa
+        '''
+        return np.dot(bd_corrections_at_qp, self.ksi_ua[material_id])
