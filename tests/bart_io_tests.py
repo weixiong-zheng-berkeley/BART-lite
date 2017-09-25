7d26ffc2e2471f1ce02303ffc311c1c08dbdb306
Joshua Rehak <jsrehak@gmail.com>
2017-09-25 11:21:43 -0700

    added test material xml file and mat_io object test setup

--- tests/bart_io_tests.py
+++ tests/bart_io_tests.py
@@ -3,6 +3,11 @@ from bart_io import mat_io
 
 class TestClass:
 
+    @classmethod
+    def setup_class(cls):
+        filename = './tests/testData/test_mat.xml'
+        cls.testMat = mat_io(filename)
+    
     @raises(AssertionError)
     def test_mat_io_nofile(self):
         """ mat_io function returns error if file is no found """
