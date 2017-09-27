0d39fbc53d0629262038654f02636ef43168bcaf
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2017-09-27 15:01:07 -0700

    Moved elementary matrix computation outside loop.

--- fempoi2d.py
+++ fempoi2d.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse as sps 
 from scipy.sparse.linalg import spsolve
 
-def fempoi2d(h, domain_length, t):
+def fempoi2d1(h, domain_length, t):
     """Solves 2D Poisson's equation for elements of size h 
     and bilinear basis equations"""
     # Define Global Matrices
@@ -14,45 +14,31 @@ def fempoi2d(h, domain_length, t):
     b = sps.lil_matrix((nodes, 1))
 
     # Find Local Basis Functions
+    # Set up Vandermonde Matrix
+    V = np.array([[1, 0, 0, 0],
+                  [1, 0, h, 0],
+                  [1, h, 0, 0],
+                  [1, h, h, h**2]])
+    C = np.linalg.inv(V)
+        
+    # Assemble Elementary Matrix
+    # A_ab = area(partial_ax*partial_bx + partial_ay*partial_by)
+    area = h**2
+    elem_A = np.zeros((4, 4))
+    elem_b = np.zeros(4)
+    for ii in range(4):
+        for jj in range(4):
+            partial_ax = C[1, ii]+C[3, ii]*V[ii, 2]
+            partial_bx = C[1, jj]+C[3, jj]*V[jj, 2]
+            partial_ay = C[2, ii]+C[3, ii]*V[ii, 1]
+            partial_by = C[2, jj]+C[3, jj]*V[jj, 1]
+            elem_A[ii,jj] = area*(partial_ax*partial_bx + partial_ay*partial_by)
+            elem_b[ii]=area
+    
     for cell in range(n):
         cell_i = cell//int(np.sqrt(n))
         cell_j = cell%int(np.sqrt(n))
-        
-        
-        # Retrieve Values for Nodes of Cell
-        x_lower = t[cell_i, cell_j][1][0]
-        x_upper = t[cell_i, cell_j][1][1]
-        y_lower = t[cell_i, cell_j][1][2]
-        y_upper = t[cell_i, cell_j][1][3]
-        
-        V = np.eye(4)
-        C = np.array([[1, x_lower, y_lower, x_lower*y_lower],
-                      [1, x_lower, y_upper, x_lower*y_upper],
-                      [1, x_upper, y_lower, x_upper*y_lower],
-                      [1, x_upper, y_upper, x_upper*y_upper]])
-        
-        # Solve for Coefficients
-        c1 = np.linalg.inv(C).dot(V[0])
-        c2 = np.linalg.inv(C).dot(V[1])
-        c3 = np.linalg.inv(C).dot(V[2])
-        c4 = np.linalg.inv(C).dot(V[3])
-        c = np.stack((c1, c2, c3, c4), axis=-1)
-        
-        
-        # Assemble Elementary Matrix
-        # A_ab = area(partial_ax*partial_bx + partial_ay*partial_by)
-        area = h**2
-        elem_A = np.zeros((4, 4))
-        elem_b = np.zeros(4)
-        for ii in range(4):
-            for jj in range(4):
-                partial_ax = c[1, ii]+c[3, ii]*C[ii, 2]
-                partial_bx = c[1, jj]+c[3, jj]*C[jj, 2]
-                partial_ay = c[2, ii]+c[3, ii]*C[ii, 1]
-                partial_by = c[2, jj]+c[3, jj]*C[jj, 1]
-                elem_A[ii,jj] = area*(partial_ax*partial_bx + partial_ay*partial_by)
-                elem_b[ii]=area/4.0*(4*c[0, ii] + h*(2*c[1, ii]+2*c[2, ii]+h*c[3, ii]))
-        
+
         # Assemble Global Matrix
         # Global to Local node mapping
         """Nodes are flattened by row with row x=0 going first.
