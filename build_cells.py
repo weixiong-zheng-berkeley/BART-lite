78c3772cc2d2093f94daa4b841e9c87a018815f0
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2017-10-02 16:02:31 -0700

    Encryped CELL_AREA

--- build_cells.py
+++ build_cells.py
@@ -14,9 +14,10 @@ CELLS = problem["mesh_cells"]
 # Calculate domain and cell lengths
 DOMAIN_LENGTH = DOMAIN_UPPER - DOMAIN_LOWER
 CELL_LENGTH = DOMAIN_LENGTH/CELLS
+CELL_AREA = CELL_LENGTH**2
 
 # Initialize Cells, MetaData, and Coordinates
-Cell=namedtuple("Cell", ["i", "j"]) 
+Cell=namedtuple("Cell", ["i", "j"])
 MetaData=namedtuple("MetaData", ["mat_id", "coordinates", "cross_sections"])
 Coordinates=namedtuple("Coordinates", ["x_lower", "x_upper", "y_lower", "y_upper"])
 
@@ -36,6 +37,6 @@ def index_to_coordinates(cell):
 
 def metadata(cell):
   return MetaData(None, index_to_coordinates(cell), None)
-  # Note: Has no mat id or cross section info. TODO. 
+  # Note: Has no mat id or cross section info. TODO.
 
 cell_to_metadata = {c:metadata(c) for c in cells}
