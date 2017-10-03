hotfix: fixed calculation of chi_nu_sig_f

material.py
@@ -111,8 +111,17 @@ class _mat():
     def __derive_fiss__(self):
         # Calculate derived quantities based on fission properties
 
-        chi_nu_sig_f = np.multiply(self.gconst['chi'],
-                                   self.prop['nu']*self.xsec['sig_f'])
+        if 'chi' in self.gconst and 'sig_f' in self.xsec:
+            vec_2 = self.prop['nu']*self.xsec['sig_f']
+        elif 'nu_sig_f' in self.xsec:
+            vec_2 = self.xsec['nu_sig_f']
+        elif 'nu_sig_f' in self.gconst:
+            vec_2 = self.gconst['nu_sig_f']
+        else:
+            vec_2 = np.array([0,0])
+            
+        chi_nu_sig_f = np.outer(self.gconst['chi'], vec_2)
+                                       
         self.derived.update({'chi_nu_sig_f': chi_nu_sig_f})
 
     def __derive_thermal__(self):
@@ -170,6 +179,8 @@ class _mat():
             self.__dict_add__(self.xsec, el)
 
         if 'nu' in self.prop and 'sig_f' in self.xsec:
+            self.isSource = True
+        elif 'nu_sig_f' in self.xsec or 'nu_sig_f' in self.gconst:
             self.isSource = True
         else:
             self.isSource = False

tests/material_tests.py
@@ -1,5 +1,4 @@
 from nose.tools import *
-from material import material
 from material import _mat
 import numpy as np
 
@@ -76,7 +75,7 @@ class TestFunctionality:
     def test_mat_calc_nu_sigf(self):
         """ Calculated nu_sigf should be correct value """
         ok_(np.allclose(self.testmat.derived['chi_nu_sig_f'],
-                        np.array([28.75, 103.5])),
+                        np.array([[28.75, 34.5], [86.25, 103.5]])),
             "Chi_nu_sig_f should be the correct value")
 
     def test_mat_no_xsec_no_chi_nu_sig_f(self):