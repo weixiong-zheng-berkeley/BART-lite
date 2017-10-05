d707eae7f50627a6777af0bf2dbeea6b9af443c2
Joshua Rehak <jsrehak@gmail.com>
2017-10-05 16:47:35 -0700

    run builds mat_lib and mat_map

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from __future__ import division
-from input import problem
 from mesh import mesh_gen
 import matplotlib.pyplot as plt
 import build_cells
 import fempoi2d
+import material
+
+#Specify problem here:
+from input_kaist_mox_1 import problem
 
 DOMAIN_LOWER = build_cells.DOMAIN_LOWER
 DOMAIN_UPPER = build_cells.DOMAIN_UPPER
@@ -13,6 +16,20 @@ DOMAIN_LENGTH = DOMAIN_UPPER - DOMAIN_LOWER
 CELL_LENGTH = DOMAIN_LENGTH/CELLS
 
 DATA = build_cells.cell_to_metadata
+
+# Build Material Library
+
+try:
+  MAT_LIB = material.mat_lib(n_grps = problem['groups'],
+                             files = problem['materials'],
+                             tr_scatt = problem['tr_scatt'])
+except KeyError:
+  MAT_LIB = material.mat_lib(n_grps = problem['groups'],
+                             files = problem['materials'])
+
+MAT_MAP = material.mat_map(lib = MAT_LIB, layout = problem['layout'],
+                           layout_dict = problem['layout_dict'],
+                           x_max = DOMAIN_UPPER, n=problem['mesh_cells'])
 
 def run():
   u = fempoi2d.fempoi2d(CELL_LENGTH, DOMAIN_LENGTH, DATA)
@@ -24,6 +41,6 @@ def run():
   plt.axis('equal')
   plt.show()
 
-run()
+#run()
 
 
