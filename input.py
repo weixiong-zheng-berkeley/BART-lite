0e7523e96c349b0d9ba078532ae958a777b65cbb
Joshua Rehak <jsrehak@gmail.com>
2017-10-05 16:47:56 -0700

    added initial kaist input file, removed old input.py

--- input.py
+++
@@ -1,7 +0,0 @@
-problem = {
-  "mesh_cells": 20,
-  "groups": 1,
-  "quadrature": 2,
-  "moments": 4,
-  "unknowns": 2
-}
---
+++ input_kaist_mox_1.py
@@ -0,0 +1,45 @@
+from os import listdir
+from os.path import isfile, join
+
+mat_dir = './mat/kaist/' # Location of material files
+mats = [mat_dir + f for
+        f in listdir(mat_dir) if isfile(join(mat_dir, f))]
+
+layout_mox1 = """
+  43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43
+  43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43
+  70 70 70 70 70 70 70 70 70 70 70 70 70 43 43 43 43
+  70 70 70 70 70 70 70 70 70 70 70 70 70 43 43 43 43
+  gt 70 70 70 70 gt gt 70 70 70 70 70 70 70 70 43 43
+  gt 70 70 70 70 gt gt 70 70 70 70 70 70 70 70 43 43
+  70 70 70 70 70 70 70 70 70 gt gt 70 70 70 70 43 43
+  70 70 70 70 70 70 70 70 70 gt gt 70 70 70 70 43 43
+  87 87 87 87 87 87 87 aa aa 70 70 70 70 70 70 43 43
+  87 87 87 87 87 87 87 aa aa 70 70 70 70 70 70 43 43
+  gt 87 87 87 87 gt gt 87 87 70 70 gt gt 70 70 43 43
+  gt 87 87 87 87 gt gt 87 87 70 70 gt gt 70 70 43 43
+  87 87 87 bb bb 87 87 87 87 70 70 70 70 70 70 43 43
+  87 87 87 bb bb 87 87 87 87 70 70 70 70 70 70 43 43
+  87 87 87 87 87 87 87 87 87 70 70 70 70 70 70 43 43
+  87 87 87 87 87 87 87 87 87 70 70 70 70 70 70 43 43
+  gt 87 87 87 87 gt gt 87 87 70 70 gt gt 70 70 43 43
+"""
+layout_dict={'43': 'mox_43',
+             '70': 'mox_70',
+             '87': 'mox_87',
+             'gt': 'guide_tube',
+             #'aa': 'mox_70',
+             'aa': 'control_rod',
+             'bb': 'mox_87'}
+
+problem = {
+    "mesh_cells": 20,
+    "groups": 7,                # REQ: number of energy groups
+    "quadrature": 2,
+    "moments": 4,
+    "unknowns": 2,
+    "materials": mats,          # REQ: list of xml material files
+    "layout": layout_mox1,      # REQ: material layout to use
+    "layout_dict": layout_dict, # REQ: material layout dictionary
+    "tr_scatt": True            # OP:  Take transp. of scatt. matrices
+}
