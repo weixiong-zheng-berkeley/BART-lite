4403b0f868897b61ba6b2f450be90fca3d1febcf
Joshua Rehak <jsrehak@gmail.com>
2017-10-06 14:14:02 -0700

    added calculation of cell area and global_idx

--- mesh.py
+++ mesh.py
@@ -9,8 +9,57 @@ def mesh_gen(cells):
         XY[i, j, 1] = xx[j]
   return XY
 
-class Cell():
+class Cell(object):
   """ A single cell in the mesh, holds location and material data """
 
-  def __init__(self):
+  def __init__(self, index, mesh_params, bound=None, mat_lib=None):
+    """ Cell constructor, give index in a tuple (i,j) """
+    
+    # Constructor validations
+    assert isinstance(index, tuple), "Index must be a tuple"
+    assert len(index) == 2, "Index must be a length 2 tuple"
+    
+    try:
+      assert mesh_params['cell_length'] > 0, "cell_length must be greater than 0"
+    except KeyError:
+      raise KeyError("Missing 'cell_length' parameter in mesh_params")
+    
+    self._index  = index
+
+    try:
+      self._length = float(mesh_params['cell_length'])
+      self._area   = np.power(self._length, 2)
+    except ValueError:
+      raise TypeError("cell_length parameter must be a number")
+
+    # Calculate global_idx
+    x_node = mesh_params['x_cell'] + 1
+    i,j = index[0], index[1]
+    self._global_idx = [x_node*i + j,
+                        x_node*i + j + 1,
+                        x_node*(i + 1) + j,
+                        x_node*(i + 1) + j + 1]
+    
+    # Get material properties
+    if mat_lib:
+      self.__material_props__(mat_lib)
+
+    # Determine if on a boundary
+    
+
+  # UTILITY FUNCTIONS ================================================
+
+  def __material_props__(self, mat_lib):
     pass
+    
+  # Getters ==========================================================
+    
+  def area(self):
+    return self._area
+   
+  def global_idx(self):
+    """ Returns global index, a list of the node indices """
+    return self._global_idx
+
+  def length(self):
+    return self._length
--- tests/cell_tests.py
+++ tests/cell_tests.py
@@ -7,6 +7,43 @@ class TestCells:
 
     @classmethod
     def setup_class(cls):
-        pass
+        cls.index = (1,1)
+        cls.mesh_params = {'n_cell': 16,
+                           'x_cell': 4,
+                           'y_cell': 4,
+                           'cell_length': 2.5}
+        cls.cell = Cell(cls.index, cls.mesh_params)
 
-    
+    def test_area(self):
+        """ Cell should be the correct area """
+        eq_(self.cell.area(), 2.5**2, "area value")
+        eq_(self.cell.length(), 2.5, "length value")
+
+    def test_global_index(self):
+        """ Given an index, constructor should generate the correct
+        global indexes """
+        global_idx = [6, 7, 11, 12]
+        ok_(type(self.cell.global_idx()) == list, "returns list")
+        eq_(self.cell.global_idx(), global_idx, "correct value")
+        
+    @raises(AssertionError)
+    def test_init_index(self):
+        bad_cell = Cell(5, self.mesh_params)
+
+    @raises(AssertionError)
+    def test_length_neg(self):
+        bad_params = self.mesh_params
+        bad_params['cell_length'] = -1
+        bad_cell = Cell(self.index, bad_params)
+
+    @raises(TypeError)
+    def test_length_wrong_type(self):
+        bad_params = self.mesh_params
+        bad_params['cell_length'] = 'c'
+        bad_cell = Cell(self.index, bad_params)
+        
+    @raises(KeyError)
+    def test_length_missing(self):
+        bad_params = self.mesh_params
+        del bad_params['cell_length']
+        bad_cell = Cell(self.index, bad_params)
