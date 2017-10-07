c9b52936d75a40401c2c8597a9ce5e40f5e335de
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2017-10-07 14:01:20 -0700

    Added boundary related info to AQ
    Added internal functionality for retrieving reflective direction
    on boundary and direction projection on boundary side
    
    Changed aq_data keys to using strings instead of using integers
    
    Changed 'dir_prods' per direction to dictionary

--- aq.py
+++ aq.py
@@ -10,17 +10,22 @@ class AQ(object):
 
         @param sn_ord Sn angular quadrature order
         '''
+        assert sn_ord%2==0, 'SN order must be even'
         # int for sn order
-        self.sn_ord = sn_ord
-        # dictionary for containing angular quandrature directions and weights
-        self.aq_data = dict()
+        self._sn_ord = sn_ord
         # number of directions
-        self.n_dir = (sn_ord+2) * sn_ord / 2
+        self._n_dir = (sn_ord+2) * sn_ord / 2
+        # dictionary for containing angular quandrature directions and weights
+        self._aq_data = {'omega':{},'wt':{},'dir_prods':{},'n_dir':self._n_dir,
+                         'bd_angle':{},'bd_vec_n':{},'refl_dir':{}}
         # make aq data
         self._quad2d()
         # store the outward normal vectors on boundaries
-        self.aq_data['boundary_normal'] =
-        {'xmin':(-1.,0),'xmax':(1.,0),'ymin':(0,-1.),'ymax':(0,1.)}
+        self._aq_data['bd_vec_n'] =
+        {'xmin':np.array([-1.,0]),'xmax':np.array([1.,0]),
+         'ymin':np.array([0,-1.]),'ymax':np.array([0,1.])}
+        # get incident and reflective directions
+        self._boundary_info()
 
     def _quad2d(self):
         '''@brief Internal function used to calculate aq data
@@ -28,11 +33,12 @@ class AQ(object):
         @param self Reference to the class
         @return aq_data dictionary
         '''
-        ct, quad1d, solid_angle = 0, np.polynomial.legendre.leggauss(self.sn_ord), 4*pi
+        # initialize dictionary data
+        ct, quad1d, solid_angle = 0, np.polynomial.legendre.leggauss(self._sn_ord), 4*pi
         # loop over relevant polar angles
-        for m in range(self.sn_ord/2, self.sn_ord):
+        for m in xrange(self._sn_ord/2, self._sn_ord):
             # calculate number of points per level
-            n_level = 4 * (self.sn_ord - m)
+            n_level = 4 * (self._sn_ord - m)
             delta = 2.0 * pi / n_level
             # get the polar angles
             mu = quad1d[0][m]
@@ -42,11 +48,38 @@ class AQ(object):
             for i in range(n_level):
                 phi = (i + 0.5) * delta
                 ox, oy = (1-mu**2.)**0.5 * cos(phi), (1-mu**2.)**0.5 * sin(phi)
-                self.aq_data[ct] = {'omega':np.array([ox, oy]), 'wt':w,\
-                'dir_prods':(ox*ox,ox*oy,oy*ox,oy*oy)}
+                self._aq_data['omega'][ct] = np.array([ox, oy])
+                self._aq_data['wt'][ct] = w
+                self._aq_data['dir_prods'][ct] = {'oxox':ox*ox,'oxoy':ox*oy,'oyoy':oy*oy}
                 ct += 1
-        assert ct==self.n_dir "number of total directions are wrong"
-        self.aq_data['n_dir'] = ct
+        assert ct==self._n_dir, "number of total directions are wrong"
+
+    def _boundary_info(self):
+        '''@brief Internal function to produce geometry related angular info, e.g.
+        reflective angle index per boundary side and incident/outgoing angle on bd
+        '''
+        # bd_names
+        bd_names = ['xmin','ymin','xmax','ymax']
+        # boundary normal vectors
+        vn = [np.array([-1.,0]),np.array([0,-1.]),np.array([1.,0]),np.array([0,1.])]
+        self._aq_data['bd_vec_n'] = {k:v for k,v in zip(bd_names,vn)}
+        # calculate boundary angles per dir, positive/negative means outgoing/incoming
+        for bd in bd_names:
+            for i in xrange(self._n_dir):
+                vec_n,omega = self._aq_data['bd_vec_n'][bd],self._aq_data['omega'][i]
+                self._aq_data['bd_angle'][(bd,i)]=np.dot(vec_n,omega)
+                # get reflective angle
+                if self._aq_data['bd_angle'][(bd,i)]>0:
+                    self._aq_data['refl_dir'][(bd,i)]=None
+                else:
+                    # calculate reflective omega per boundary bd
+                    r_dir = omega - 2.*vec_n*np.dot(omega,vec_n)
+                    # loop over all angles
+                    for ind,r_omega in self._aq_data['omega'].items():
+                        # collect index if the angle is equal to calculated refl angle
+                        if np.array_equal(r_dir,r_omega):
+                            self._aq_data['refl_dir'][(bd,i)] = ind
+                            break
 
     def get_aq_data(self):
         '''@brief Interface function to get aq_data
@@ -54,4 +87,4 @@ class AQ(object):
         @param self Reference to the class
         @return aq_data dictionary
         '''
-        return self.aq_data
+        return self._aq_data
