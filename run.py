099ba621f6cf9ae3df10d95a49401d29330ecce1
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2017-10-11 18:30:45 -0700

    Modified run.py so equations can readin correct parameters

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from __future__ import division
 import matplotlib.pyplot as plt
 import build_cells
-import fempoi2d
 import material
 import mesh
 
@@ -11,15 +10,15 @@ from input_kaist_mox_1 import problem
 # Build Material Library
 
 try:
-  MAT_LIB = material.mat_lib(n_grps = problem['groups'],
+    MAT_LIB = material.mat_lib(n_grps = problem['groups'],
                              files = problem['materials'],
                              tr_scatt = problem['tr_scatt'])
 except KeyError:
-  MAT_LIB = material.mat_lib(n_grps = problem['groups'],
+    MAT_LIB = material.mat_lib(n_grps = problem['groups'],
                              files = problem['materials'])
 
 # Build Material Mapping
-  
+
 MAT_MAP = material.mat_map(lib = MAT_LIB, layout = problem['layout'],
                            layout_dict = problem['layout_dict'],
                            x_max=problem['domain_upper'],
@@ -30,15 +29,29 @@ MAT_MAP = material.mat_map(lib = MAT_LIB, layout = problem['layout'],
 MESH = mesh.Mesh(problem['mesh_cells'], problem['domain_upper'], MAT_MAP)
 
 def run():
-  u = fempoi2d.fempoi2d(CELL_LENGTH, DOMAIN_LENGTH, DATA)
-  x = mesh_gen(CELLS)[:, :, 0]
-  y = mesh_gen(CELLS)[:, :, 1]
-  cset1 = plt.contourf(x, y, u, 10)
-  plt.colorbar()
-  plt.contour(x, y, u, cset1.levels, hold='on', colors='k')
-  plt.axis('equal')
-  plt.show()
-
+    '''
+    u = fempoi2d.fempoi2d(CELL_LENGTH, DOMAIN_LENGTH, DATA)
+    x = mesh_gen(CELLS)[:, :, 0]
+    y = mesh_gen(CELLS)[:, :, 1]
+    cset1 = plt.contourf(x, y, u, 10)
+    plt.colorbar()
+    plt.contour(x, y, u, cset1.levels, hold='on', colors='k')
+    plt.axis('equal')
+    plt.show()
+    '''
+    # do we do NDA
+    do_nda = problem['do_nda']
+    # Eigen class construction
+    eigen_cls = Eigen()
+    # construct HO solver
+    ho_cls = SAAF(mat_lib=MAT_LIB, msh_cls=MESH, prob_dict=problem)
+    if not do_nda:
+        # eigen solving
+        eigen_cls.do_iterations(ho_cls=ho_cls, nda_cls=None)
+    else:
+        # construct NDA solver
+        nda_cls = NDA(mat_lib=MAT_LIB, msh_cls=MESH, prob_dict=problem)
+        # eigen solving
+        eigen_cls.do_iterations(ho_cls=ho_cls, nda_cls=nda_cls)
+    # TODO: output and plotting functionality
 #run()
-
-
